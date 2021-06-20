6acb0dc6fffa07c07d5d9397d2e60e2ffaeca2d3
MCBerberov19 <MCBerberov19@codingburgas.bg>
2021-06-20 11:26:49 +0300

    Add function for hiding and unhiding password

--- Website/JavaScript/login.js
+++ Website/JavaScript/login.js
@@ -1,10 +1,24 @@
 function hideAndShowPass() {
-        let view = document.getElementById("pass");
-        if (view.type === "password") {
-            view.type = "text";
-            document.getElementById("view").src = "../IMG/unview.png";
-        } else {
-            view.type = "password";
-            document.getElementById("view").src = "../IMG/view.png";
-        }
+    let view=document.getElementById("pass1");
+    if(view.type=="password")
+    {
+        document.getElementById("imgView1").src="../img/unview.png";
+        view.type="text";
+    }
+    else{
+        document.getElementById("imgView1").src="../img/view.png";
+        view.type="password";
+    }
 }
+
+function hideAndShowPassConfirm() {
+    let view=document.getElementById("pass2");
+    if (view.type === "password") {
+        view.type = "text";
+        document.getElementById("imgView2").src = "../img/unview.png";
+    } else {
+        view.type = "password";
+        document.getElementById("imgView2").src = "../img/view.png";
+    }
+}
+
