686bbe2359aa869db2e10dead703f711eb0bc002
Mario Berberov <MCBerberov19@codingburgas.bg>
2021-06-25 17:42:58 +0300

    Create menu

--- Fish Application/Fish Application/Fish Application.cpp
+++ Fish Application/Fish Application/Fish Application.cpp
@@ -1,20 +1,26 @@
-// Fish Application.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+ofstream inputData("FishData.txt");
+ifstream outputData("FishData.txt");
+bool checkForMatch = false;
+
+void printMenu()
+{
+	printf("1. Show fish data\n");
+	printf("2. Add fish data (Administration permissions needed!)\n");
+	printf("3. Remove fish data (Administration permissions needed!)\n");
+	printf("4. Sort fish data\n");
+	printf("5. Find fish data by parameters\n");
+}
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
