65369118ed766e38ee7e60b7053d813bd7532140
Veselin Stoqnov <VNStoyanov19@codingburgas.bg>
2021-06-26 21:54:59 +0300

    Add error to cards

---
+++ Website/JavaScript/index.js
@@ -0,0 +1,36 @@
+function showMore() {
+    var x = "Release date on 06.07.2021";
+    document.getElementById("cardtext").innerHTML = x;
+    document.getElementById("cardtext").style.color = "red"
+}
+
+function showLess() {
+    var y = "You can change your choice only with one click";
+    document.getElementById("cardtext").style.color = "grey"
+    document.getElementById("cardtext").innerHTML = y;
+}
+
+function showMore2() {
+    var x = "Release date on 06.07.2021";
+    document.getElementById("cardtext2").innerHTML = x;
+    document.getElementById("cardtext2").style.color = "red"
+}
+
+function showLess2() {
+    var y = "You can change your choice only with one click";
+    document.getElementById("cardtext2").style.color = "grey";
+    document.getElementById("cardtext2").innerHTML = y;
+}
+
+function showMore3() {
+    var x = "Release date on 06.07.2021";
+    document.getElementById("cardtext3").innerHTML = x;
+    document.getElementById("cardtext3").style.color = "red";
+}
+
+function showLess3() {
+    var y = "You can change your choice only with one click";
+    document.getElementById("cardtext3").style.color = "grey";
+    document.getElementById("cardtext3").style.decoration = "underline";
+    document.getElementById("cardtext3").innerHTML = y;
+}
