cdff49517155ba5d90657ff5f11a604acbb9fccb
Veselin Stoqnov <VNStoyanov19@codingburgas.bg>
2021-06-27 16:52:24 +0300

    Edit Cards' Accessibilitys

--- Website/JavaScript/index.js
+++ Website/JavaScript/index.js
@@ -1,35 +1,35 @@
 function showMore() {
-    var x = "Release date on 06.07.2021";
+    var x = "Available yet";
     document.getElementById("cardtext").innerHTML = x;
-    document.getElementById("cardtext").style.color = "red"
+    document.getElementById("cardtext").style.color = "green";
 }
 
 function showLess() {
-    var y = "You can change your choice only with one click";
-    document.getElementById("cardtext").style.color = "grey"
+    var y = "Add freshwater or saltwater fishes";
+    document.getElementById("cardtext").style.color = "grey";
     document.getElementById("cardtext").innerHTML = y;
 }
 
 function showMore2() {
-    var x = "Release date on 06.07.2021";
+    var x = "Non available yet";
     document.getElementById("cardtext2").innerHTML = x;
     document.getElementById("cardtext2").style.color = "red"
 }
 
 function showLess2() {
-    var y = "You can change your choice only with one click";
+    var y = "Huge, Big, Average and Small";
     document.getElementById("cardtext2").style.color = "grey";
     document.getElementById("cardtext2").innerHTML = y;
 }
 
 function showMore3() {
-    var x = "Release date on 06.07.2021";
+    var x = "Non available yet";
     document.getElementById("cardtext3").innerHTML = x;
     document.getElementById("cardtext3").style.color = "red";
 }
 
 function showLess3() {
-    var y = "You can change your choice only with one click";
+    var y = "Administrator can make fish data indelible";
     document.getElementById("cardtext3").style.color = "grey";
     document.getElementById("cardtext3").style.decoration = "underline";
     document.getElementById("cardtext3").innerHTML = y;
